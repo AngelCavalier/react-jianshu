646f3114ff2e2e68729163ac49e1798474d491b6
AngelCavalier <2233481591@qq.com>
2021-03-17 19:59:09 +0800

    缺少分号

--- src/common/header/style.js
+++ src/common/header/style.js
@@ -97,9 +97,9 @@ export const SearchInfo = styled.div`
   position: absolute;
   top: 56px;
   width: 240px;
-  padding 0 20px;
-  box-shadow: 0 0 8px rgba(0,0,0,.2);
-  background: #fff
+  padding: 0 20px;
+  box-shadow: 0 0 8px rgba(0, 0, 0, 0.2);
+  background: #fff;
 `;
 export const SearchInfoTitle = styled.div`
   margin-top: 20px;
